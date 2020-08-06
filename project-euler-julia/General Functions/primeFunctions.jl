24f720f37b7bb6783216a7fba9e4e07fc73e2a5c
eshanuniyal <eshanuniyal@g.ucla.edu>
2020-08-06 14:22:29 +0530

    Optimised prime-generating function

--- project-euler-julia/General Functions/primeFunctions.jl
+++ project-euler-julia/General Functions/primeFunctions.jl
@@ -1,16 +1,16 @@
-import DataStructures.IntSet
+import DataStructures   # LinkedLists
 # using TimerOutputs
 
-to = TimerOutput()
+# to = TimerOutput()
 
 function generatePrimes(bound::Int)::Vector{Int}
     # primitive, brute force algorithm
-    nPrimeBound::Int = ceil(1.25506 * bound / log(bound))
+    πₙ::Int = ceil(1.25506 * bound / log(bound))
         # a guaranteed, close upper-bound on the number of primes <= bound
 
     # creating primes vector
     primes::Vector{Int} = [2]
-    sizehint!(primes, nPrimeBound)  # preallocating storage
+    sizehint!(primes, πₙ)  # preallocating storage
 
     # iterating over odd numbers
     for num in 3:2:bound
@@ -32,21 +32,39 @@ function generatePrimes(bound::Int)::Vector{Int}
 end
 
 function sieve(n::Int)::Vector{Int}
+    # primitive sieve of Eratosthenes
+    # primes up to 10^8 in ~2 seconds
+
+    πₙ = Int(ceil(1.25506 * n / log(n)))
+        # a guaranteed, close upper-bound on the number of primes <= bound
+
+    # initialising primes vector
+    primes::Vector{Int} = []
+    sizehint!(primes, πₙ)
+
+    # initialising numbers vector (sieve)
     numbers::Vector{Bool} = fill(true,n)
     numbers[1] = false
+
+    # iterating over numbers to find primes
     for i in 2:isqrt(n)
+        # numbers[i] == true -> i is a prime
         if numbers[i]
-            for j = 2*i:i:n
-            numbers[j] = false
+            # inserting in primes
+            push!(primes, i)
+            # sieving multiples
+            for j in 2*i:i:n
+                numbers[j] = false
             end
         end
     end
-    primes = findall(x -> x, numbers)
+
+    # return primes vector
     return primes
 end
 
 function main()
-    @time sieve(10^4)
+    @time println(length(sieve(10^8)))
 end
 
 main()
