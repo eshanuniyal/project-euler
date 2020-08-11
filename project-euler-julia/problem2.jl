f628d8267ea2717821f2ad8e0577a6460f8e01ca
eshanuniyal <eshanuniyal@g.ucla.edu>
2020-08-11 22:23:34 +0530

    Updated variable names to idiomatic Julia

--- project-euler-julia/problem2.jl
+++ project-euler-julia/problem2.jl
@@ -3,23 +3,23 @@
 
 function findEvenFibonacciSum(bound::Int)::Int
 
-    total = 0  # initialising sum variable
+    ∑ = 0  # initialising sum variable
 
     # initialising base cases
-    Fᵢ, Fⱼ = 0, 1
-    # iterating until we reach bound
-    while Fᵢ + Fⱼ ≤ bound
+    Fᵢ₋₁, Fᵢ = 0, 1
+    # iterating until we reach bound 
+    while Fᵢ₋₁ + Fᵢ ≤  bound
         # generating next Fibonacci number
-        Fₖ = Fᵢ + Fⱼ
+        Fᵢ₊₁ = Fᵢ₋₁ + Fᵢ
         # adding to total if even
-        if Fₖ % 2 === 0
-            total += Fₖ
+        if Fᵢ₊₁ % 2 == 0
+            ∑ += Fᵢ₊₁
         end
         # updating state variables
-        Fᵢ, Fⱼ = Fⱼ, Fₖ
+        Fᵢ₋₁, Fᵢ = Fᵢ, Fᵢ₊₁
     end
 
-    return total
+    return ∑
 end
 
 
