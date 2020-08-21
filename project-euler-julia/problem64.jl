fd1a9d1bc1236f08c46ec0f06f0bdb866ce9693d
eshanuniyal <eshanuniyal@g.ucla.edu>
2020-08-21 13:57:35 +0530

    Added solution to Problem 64

---
+++ project-euler-julia/problem64.jl
@@ -0,0 +1,27 @@
+# Project Euler Problem 64 (https://projecteuler.net/problem=64)
+# July 20, 2020
+# Runtime: 10⁻² seconds
+
+import AuxFunctions: findContinuedFraction
+
+"""
+    oddPeriodSquareRoots(bound)
+
+Returns the number of continued fractions of `√n` for `n ≤ bound` that have an odd period. 
+"""
+function oddPeriodSquareRoots(bound::Integer)
+	
+	oddCount = 0
+
+	# finding continued fractions with odd period
+    for N in 1:bound
+        # check if continued fraction for √N has odd period
+        length(findContinuedFraction(N)) % 2 == 1 && (oddCount += 1)
+            # note: for perfect squares, findContinuedFraction returns empty vector (even period)
+    end
+
+	return oddCount
+end
+
+# function call
+@time println(oddPeriodSquareRoots(10000))
