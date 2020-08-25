e9a8f4fa5f3edc66476341d34fba13f28fee3376
eshanuniyal <eshanuniyal@g.ucla.edu>
2020-08-26 00:35:45 +0530

    Added solution to Problem 203

---
+++ project-euler-julia/problem203.jl
@@ -0,0 +1,47 @@
+# Julia Solution to Project Euler Problem 203
+# 25 August 2020
+# Runtime: 10⁻³ seconds
+
+import PrimeFunctions: sieve
+
+"""
+    squareFree(n, primes, primesSet)
+"""
+function isSquareFree(n, primes, primesSet)
+
+    nₜ = n  # temporary n variable (for finding prime factors)
+    # iterating over primes
+    for p in primes
+        # reduce nₜ if nₜ is divisible by prime p
+        if nₜ % p == 0
+            nₜ ÷= p
+            # return false if nₜ is further divisible by p
+            nₜ % p == 0  && return false
+            # in either case n == 1 or nₜ ∈ primeSet, nₜ cannot be divisible by a prime-square
+            (nₜ == 1 || nₜ ∈ primesSet) && break
+        end
+    end
+
+    # n passed square-free test ⟹ return true
+    return true
+end
+
+function squareFreeBinomialCoefficients(nRows)
+
+    # defining binomial function
+    bin(n, k) = factorial(big(n)) ÷ (factorial(big(k)) * factorial(big(n - k)))
+
+    # generating all unique binomial coefficients for n < nRows
+    coeffs = Set([bin(n, k) for n in 1:nRows-1 for k in 0:n÷2])
+    # generating enough primes to check whether coefficients are square free
+    primes = Int(coeffs |> maximum |> isqrt) |> sieve
+        # if k is not square-free, it is divisible by p² for some prime p
+    # creating set of primes (for fast checking of primality)
+    primesSet = Set(primes)
+
+    # returning sum of binomial coefficients that are squarefree
+    return filter(c -> isSquareFree(c, primes, primesSet), coeffs) |> sum
+end
+
+# function call and benchmarking
+@time squareFreeBinomialCoefficients(51)
